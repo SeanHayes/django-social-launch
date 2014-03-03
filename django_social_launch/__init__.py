dd0f9c25402393c52108bb0a5a0a0c47d9169b79
Seán Hayes <sean@seanhayes.name>
2014-03-03 02:36:41 -0500

    Added __version__

--- django_social_launch/__init__.py
+++ django_social_launch/__init__.py
@@ -1,3 +1,7 @@
+VERSION = (0, 0, 1)
+
+__version__ = "".join([".".join(map(str, VERSION[0:3])), "".join(VERSION[3:])])
+
 user_successfully_created_msg = 'You successfully edited your profile.'
 
 referrer_url_session_key = 'social_launch_referrer_url'
