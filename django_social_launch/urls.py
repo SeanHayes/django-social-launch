df49b4789ed729004198a33f61f6b540468279c7
Sean Hayes <sean@seanhayes.name>
2015-03-26 16:32:44 -0400

    Change to url path, cleaned up HTML.

--- django_social_launch/urls.py
+++ django_social_launch/urls.py
@@ -7,5 +7,5 @@ from .views import index
 # place app url patterns here
 urlpatterns = patterns('',
     url(r'^$', index, name='social_launch_index'),
-    url(r'^(?P<referring_user_pk>.+)/$', index, name='social_launch_referral'),
+    url(r'^referrers/(?P<referring_user_pk>.+)/$', index, name='social_launch_referral'),
 )
